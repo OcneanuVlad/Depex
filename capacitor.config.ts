a694d246c31191ff3781dd7926234fb3e08654c0
Ocneanu Vlad <102417094+OcneanuVlad@users.noreply.github.com>
2024-04-25 18:59:09 +0300

    depex

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.norocosul.app',
-  appName: 'Norocosul Castigator',
-  webDir: 'dist'
-};
-
-export default config;
