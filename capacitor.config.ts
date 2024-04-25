fdf43b11a8df154643b8a6bb9e3ed675cd99ecc9
Ocneanu Vlad <ocneanuvlad07@gmail.com>
2024-04-25 15:37:17 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-iframe-test',
+  webDir: 'dist'
+};
+
+export default config;
